84d900a89da6df52dfba001e0e318e0f8d66e92c
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2019-05-07 15:00:05 +0200

    Udpate factorial

--- factorial/factorial_2.py
+++ factorial/factorial_2.py
@@ -2,13 +2,11 @@ def factorial(n: int) -> int:
     if not isinstance(n, int):
         raise Exception("n must be an integer!")
 
-    if n < 0:
-        raise Exception("n must be positive!")
+    # Missing: check if n > 0
 
     fac: int = 1
 
-    # Error: it should be n + 1
-    for i in range(1, n):
+    for i in range(1, n + 1):
         fac *= i
 
     return fac
